dc86acd19f724e1109589610fc8c8b1ada796dc2
Echo <mbm1216@163.com>
2022-10-22 01:32:37 +0800

    conflict-init-01

--- 20200104-springcloud-zuul-management/20200108-springcloud-zuul-5081/src/main/java/com/aaa/mbm/springcloud/ApplicationRun5081.java
+++ 20200104-springcloud-zuul-management/20200108-springcloud-zuul-5081/src/main/java/com/aaa/mbm/springcloud/ApplicationRun5081.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @program: 20200104-springcloud-ribbo-custom
@@ -17,6 +18,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@ComponentScan
 public class ApplicationRun5081 {
 
     public static void main(String[] args) {
